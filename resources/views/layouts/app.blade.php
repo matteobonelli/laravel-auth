23e433c7c5169832cdb1fcb1c7a99c491a39e5b6
matteobonelli <matteobonelli08@gmail.com>
2024-01-14 23:34:36 +0100

    added projects voice on navbar (you can access only if you are logged in)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('/') }}">{{ __('Home') }}</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{url('/admin/projects') }}">{{ __('Progetti') }}</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
